45d9187744cdae3df478765b39ea953fde5fdef4
cvazy <player7712@mail.ru>
2024-12-28 23:52:55 +0300

    Реализовал главную страницу приказов

---
+++ src/app/orders/new/page.tsx
@@ -0,0 +1,9 @@
+const NewOrderPage = () => {
+  return (
+    <div className={"flex flex-col items-center gap-12 w-full"}>
+      Новый отчёт
+    </div>
+  );
+};
+
+export default NewOrderPage;
--- src/components/Sidebar/ui/MenuItem/ui/index.tsx
+++ src/components/Sidebar/ui/MenuItem/ui/index.tsx
@@ -8,17 +8,22 @@ import { IMenuItem } from "@/components/Sidebar/model";
 export const MenuItem: FC<IMenuItem> = ({ link, name }) => {
   const pathname = usePathname();
 
+  const isActive =
+    link === "/"
+      ? pathname === link
+      : pathname.startsWith(link) && pathname !== "/";
+
   return (
     <Link
       href={link}
-      className={`flex items-center gap-[16px] flex-nowrap text-black ${pathname !== link ? "pl-[31px]" : ""} min-h-[29px] w-full`}
+      className={`flex items-center gap-[16px] flex-nowrap text-black ${!isActive ? "pl-[31px]" : ""} min-h-[29px] w-full`}
     >
       <div
-        className={`${pathname === link ? "block" : "hidden"} bg-main_purple min-w-[15px] max-w-[15px] min-h-[15px] max-h-[15px] rounded-full`}
+        className={`${isActive ? "block" : "hidden"} bg-main_purple min-w-[15px] max-w-[15px] min-h-[15px] max-h-[15px] rounded-full`}
       ></div>
 
       <p
-        className={`text-2xl text-left font-bold ${pathname === link ? "text-main_purple" : "text-main_gray"}`}
+        className={`text-2xl text-left font-bold ${isActive ? "text-main_purple" : "text-main_gray"}`}
       >
         {name}
       </p>
